d1943de8aad4aff496a63198e575c580684dfe13
dowhileNodes <77521013+dowhileNodes@users.noreply.github.com>
2021-01-18 18:27:26 +0800

    pre-heroku

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,30 +1,11 @@
 @extends('layout')
 @section('content')
 @if(Session::get('status'))
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+
 <div class="alert alert-success alert-dismissible fade show" role="alert">
-  {{Session::get('status')}}
+  {{Session::get('status')}}<nav class="navbar navbar-light bg-light">
+  <span class="navbar-brand mb-0 h1">Navbar</span>
+
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
@@ -82,4 +63,5 @@
 							</div>
 						</div>
 					</div>
+	</nav>
 					@stop
